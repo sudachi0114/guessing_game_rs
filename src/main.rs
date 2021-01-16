bfd6e6942c4f2e65bde00088ca0133858e08963a
sudachi0114 <jnevariste@gmail.com>
2021-01-16 19:37:46 +0900

    implement user input and program output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+	println!("Please input your guess.");
+
+	let mut guess = String::new();
+
+	io::stdin().read_line(&mut guess)
+		.expect("Failed to read line");  // 入力の読み込みに失敗
+
+	println!("You guessed: {}", guess);
 }
+
+
