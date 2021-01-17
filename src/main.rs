036c47eddbb330b6239a4b3cd43dd733999b01f1
sudachi0114 <jnevariste@gmail.com>
2021-01-17 18:46:55 +0900

    create loop while guessing process

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,24 @@ fn main() {
 	let secret_number = rand::thread_rng().gen_range(1, 101);
 	println!("[info] The secret_number is {}", secret_number);
 
-	println!("Please input your guess.");
+	loop{
+		println!("Please input your guess.");
 
-	let mut guess = String::new();
+		let mut guess = String::new();
 
-	io::stdin().read_line(&mut guess)
-		.expect("Failed to read line");  // 入力の読み込みに失敗
+		io::stdin().read_line(&mut guess)
+			.expect("Failed to read line");  // 入力の読み込みに失敗
 
-	// shadowing
-	let guess: u32 = guess.trim().parse()
-		.expect("Please type a number!");
+		// shadowing
+		let guess: u32 = guess.trim().parse()
+			.expect("Please type a number!");
 
-	println!("You guessed: {}", guess);
+		println!("You guessed: {}", guess);
 
-	match guess.cmp(&secret_number) {
-		Ordering::Less => println!("Too small!"),
-		Ordering::Greater => println!("Too big!"),
-		Ordering::Equal => println!("You win!"),
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => println!("You win!"),
+		}
 	}
 }
